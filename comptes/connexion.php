a37434315fac2c6e26fedb78f6a16f8b748c76e9
Manganese92 <103360340+Manganese92@users.noreply.github.com>
2022-05-14 11:20:30 +0200

    redirection

--- comptes/connexion.php
+++ comptes/connexion.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./styles/index_connexion.css">
+    <link rel="stylesheet" href="./styles/connexion.css">
   </head>
 
 <body>
@@ -25,10 +25,11 @@
 
       <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Ce souvenir de moi
+          <input type="checkbox" value="remember-me"> Ce souvenir de moi </input>
         </label>
       </div>
       <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
     </form>
   </main>
 </body>
+</html>
